a159933de10ce8985d93c1c7becae836cd4d202b
seifarth <devnull@localhost>
2011-03-13 18:04:25 +0100

    add routes and some start files

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def impressum
+  end
+
+  def help
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Befchina::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +50,9 @@ Befchina::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
+  root :to => "pages#home"
+  match 'pages/impressum' => 'pages#impressum', :as => :impressum
+  match 'pages/help' => 'pages#help', :as => :help
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
