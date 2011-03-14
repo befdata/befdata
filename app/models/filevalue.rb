8c27fb193d8b734e985968ee3f3ddf3d44e35cd3
seifarth <devnull@localhost>
2011-03-14 11:46:12 +0100

    add paperclip to filevalue, also taggable

--- app/models/filevalue.rb
+++ app/models/filevalue.rb
@@ -1,2 +1,22 @@
 class Filevalue < ActiveRecord::Base
+
+
+
+  validates_uniqueness_of :file_file_name
+
+  has_attached_file :file,
+  :basename => "basename",
+  :path => ":rails_root/files/:filename",
+  :url => "files/:id/download"
+
+  # tagging
+  is_taggable :tags, :languages
+
+#  before_destroy :check_for_measurements
+#
+# measurements will not be linked to filevalues, there will be a
+# separate table for uploading free format files
+
+  after_destroy :destroy_taggings
+
 end
