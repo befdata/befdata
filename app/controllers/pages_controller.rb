1fa5f8d964a173a4ed1c57611d88edcff7e81a73
seifarth <devnull@localhost>
2011-03-14 10:30:15 +0100

    add dashboard method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,14 @@ class PagesController < ApplicationController
   def help
   end
 
+  # This method is the dashboard method of our Portal
+  # This provide a first look to our metadata and give a hint about our data
+  def data
+
+
+    @tags = Tag.find(:all, :order => :name)
+    @datasets = DataSet.find(:all, :order => :title)
+    
+  end
+
 end
