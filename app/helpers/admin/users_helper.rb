1dc8ea2685c361a5f8a03d39e7ac3c4c4ffaddaa
seifarth <devnull@localhost>
2011-03-23 14:54:57 +0100

    remove not needed code

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -5,11 +5,6 @@ module Admin::UsersHelper
         map{|role_object| role_object.name}.join(", ")
   end
 
-#
-#  def omnipotent_form_column(record, name)
-#    check_box :record, :omnipotent, :name => name
-#  end
-
   def roles_with_objects_column(record)
     record.role_objects.reject{|role_object| role_object.authorizable_type.nil?}.
         map{|role_object| "#{role_object.name} of #{role_object.authorizable_type} with id: #{role_object.authorizable_id}"}.join(", ")
