16d0fcf3ab014768bafbc9d5ddf9270ecdad8df2
seifarth <devnull@localhost>
2011-05-20 13:41:31 +0200

    fixed test for view of one tag

--- test/functional/tags_controller_test.rb
+++ test/functional/tags_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TagsControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, :id => 1
     assert_response :success
   end
 
