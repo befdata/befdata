253fa6bd716aed351a836e23b7385a3f21c576c5
seifarth <devnull@localhost>
2011-05-20 13:40:13 +0200

    ref #4538 Add own database configuration (looks into database.dist.yml) rake db:setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+
+if Rails.env="performance"
+  i = 0
+  p "Creating 5000 users and datasets"
+  while i < 5000
+    dataset = Dataset.new
+    person = User.new
+    person.login = "test#{i}"
+    person.email = "test#{i}@email.de"
+    person.crypted_password = "#{i}"
+    person.password_salt = "#{i}"
+    person.persistence_token = "#{i}"
+    person.single_access_token = "#{i}"
+    person.perishable_token = "#{i}"
+    person.lastname = "Test#{i}"
+    person.firstname = "Test#{i}"
+    dataset.save(:validate => false)
+    person.save(:validate => false)
+    person.has_role! :owner, dataset
+    p i = i + 1
+  end
+
+  person = User.first
+  i = 0
+  p "Creating 5000 roles for first user to all datasets"
+  while i < 4000
+    dataset = Dataset.find(i+1)
+    person.has_role! :owner, dataset if dataset
+    p i = i + 1
+  end
+end
