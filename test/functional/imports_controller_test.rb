66d912ab15ba4afa5f6e6573c962425098f5817f
seifarth <devnull@localhost>
2011-05-30 09:14:16 +0200

    first filevalue import test

--- test/functional/imports_controller_test.rb
+++ test/functional/imports_controller_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
+require "authlogic/test_case"
 
 class ImportsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup :activate_authlogic
+
+  test "creating freeformat dataset and deleting should work" do
+    # Arrange
+    nadrowski = users(:users_006)
+    UserSession.create(nadrowski)
+
+    freeformat = {:file => File.new(File.join(fixture_path, 'files', 'People.ppt'))}
+
+    # Act
+    post(:create_dataset_freeformat, {:freeformat => freeformat})
+
+    #Assert
+    assert_response :redirect
   end
 end
