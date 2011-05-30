928e3082205063a62074c9074eb2ffe5a1253f03
seifarth <devnull@localhost>
2011-05-30 09:14:34 +0200

    second dataset freeformat upload test

--- test/functional/datasets_controller_test.rb
+++ test/functional/datasets_controller_test.rb
@@ -1,17 +1,23 @@
 require 'test_helper'
+require "authlogic/test_case"
 
 class DatasetsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  setup :activate_authlogic
 
-  test "creating freeformat dataset and deleting should work" do
+  test "after create freeformat file new dataset should  created" do
   # Arrange
+  nadrowski = users(:users_006)
+  UserSession.create(nadrowski)
+
+  freeformat_file = {:file => File.new(File.join(fixture_path, 'files', 'empty_test_file.txt'))}
+  freeformat = Freeformat.create(freeformat_file)
 
   # Act
+  get(:upload_dataset_freeformat, :freeformat_id => freeformat.id)
 
   #Assert
+  assert_response :success
+  assert_select 'div#content', /empty_test_file.txt/
   end
 
   test "download freeformat dataset should work" do
@@ -27,6 +33,6 @@ class DatasetsControllerTest < ActionController::TestCase
 
   # Act
 
-  #Assert  
+  #Assert
   end
 end
