499654f109c44730845b1d93e5c2e8c993299eae
prilop <devnull@localhost>
2011-06-07 18:10:25 +0200

    + added shellscript for db and files backup

---
+++ ext/database-and-file-backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ROOT=$PWD/../..
+BACKUP_FOLDER=$ROOT/backup
+
+export RAILS_ENV=staging
+export RUBYOPT=
+
+SUFFIX=`date +%Y%m%d-%H%M`
+DB_USER=seifarth
+DB_PASSWORD=xxxxxxx
+DB_NAME=befchina-staging
+
+echo "Used Ruby = `which ruby`"
+
+pushd $ROOT
+  echo "Backup database"
+  if [ ! -d $BACKUP_FOLDER ]; then mkdir $BACKUP_FOLDER; fi
+
+  mysqldump -u ${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} > $BACKUP_FOLDER/${DB_NAME}-${SUFFIX}.sql
+  tar -czf $BACKUP_FOLDER/${DB_NAME}-${SUFFIX}-files.tgz $ROOT/files/*
+
+popd
