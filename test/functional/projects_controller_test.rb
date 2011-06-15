6be5f43f3218bd65721dbdd7b6c176a94d524686
prilop <devnull@localhost>
2011-06-15 17:20:22 +0200

    + recent refactoring broke project view #4613

--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class ProjectsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should get show project" do
+    get Project.first
+    assert_response :success
   end
 end
