edd1e98d5361edc95a2d21f90cc08268626031f6
prilop <devnull@localhost>
2011-06-15 17:42:36 +0200

    forgot function test syntax :)

--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ProjectsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   test "should get show project" do
-    get Project.first
+    get :show, {:id => Project.first.id}
     assert_response :success
   end
 end
