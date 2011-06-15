ab58f151e92d28619cea57685c3c309a42c86f40
prilop <devnull@localhost>
2011-06-15 12:42:07 +0200

    improved backup-script

--- ext/database-and-file-backup.sh
+++ ext/database-and-file-backup.sh
@@ -4,19 +4,19 @@ ROOT=$PWD/..
 BACKUP_FOLDER=$ROOT/backup
 
 export RAILS_ENV=staging
-export RUBYOPT=
 
 SUFFIX=`date +%Y%m%d-%H%M`
-DB_USER=seifarth
+DB_USER=database_dumper
 DB_NAME=befchina-staging
 
-echo "Used Ruby = `which ruby`"
+echo "PostgreSQl Version  = `psql --version`"
+echo "PostgreSQL Dumper Version = `pg_dump --version`"
 
 pushd $ROOT
-  echo "Backup database"
   if [ ! -d $BACKUP_FOLDER ]; then mkdir $BACKUP_FOLDER; fi
-
-  pg_dump -U ${DB_USER} ${DB_NAME} > $BACKUP_FOLDER/${DB_NAME}-${SUFFIX}.sql
+  echo "Backup database"
+  pg_dump -U ${DB_USER} -W ${DB_NAME} > $BACKUP_FOLDER/${DB_NAME}-${SUFFIX}.sql
+  echo "Backup files"
   tar -czf $BACKUP_FOLDER/${DB_NAME}-${SUFFIX}-files.tgz $ROOT/files/*
 
 popd
