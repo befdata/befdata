61dd27291047a249d3095e4bbc50686b9bc0b41b
prilop <devnull@localhost>
2011-06-16 10:01:15 +0200

    + supressing annoying deprecation warnings while running tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,7 @@ Befchina::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  ActiveSupport::Deprecation.silenced = true
+
 end
