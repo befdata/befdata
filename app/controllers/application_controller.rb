b8af3fd0d43efc11737e2ff93bf054acee3396ca
seifarth <devnull@localhost>
2011-06-16 14:53:16 +0200

    if no user use the login path not the user_session/new path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ private
     unless current_user
       store_location
       flash[:notice] = "You must be logged in to access this page"
-      redirect_to new_user_session_url
+      redirect_to :login
       return false
     end
   end
