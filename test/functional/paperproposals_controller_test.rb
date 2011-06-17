06d9455d1de179e14ffb5a6f40d48ced86d3ffb7
seifarth <devnull@localhost>
2011-06-17 13:42:52 +0200

    some tests for paperproposal creation process

--- test/functional/paperproposals_controller_test.rb
+++ test/functional/paperproposals_controller_test.rb
@@ -30,15 +30,43 @@ class PaperproposalsControllerTest < ActionController::TestCase
   end
 
   test "should add datasets to paperproposal and the author list is changed" do
+    login_and_load_paperproposal "Step 1 Paperproposal"
+    @dataset_with_michael = Dataset.find_by_title("Test species name import second version")
 
+    
+    get :edit, :id => @paperproposal.id
+    assert_select "div#datasets li", {:count => Dataset.count}
+    assert_select "div#author-list li#potential ul li", false
+
+    put :update, :id => @paperproposal.id, :datasets => [@dataset_with_michael.id], :aspect => {"#{@dataset_with_michael.id}" => "main"}
+    assert_redirected_to edit_paperproposal_path(@paperproposal)
+
+    get :edit, :id => @paperproposal.id    
+    assert_select "div#author-list li#potential ul", {:text=> /Michael/}
   end
 
-  test "should add file to paperproposal should work" do
 
+  
+  test "should add file to paperproposal should work" do
+    login_and_load_paperproposal "Step 1 Paperproposal"
+    paperproposal_file = {:file => File.new(File.join(fixture_path, 'test_files_for_uploads', 'empty_test_file.txt'))}
+
+
+    put :update, :id => @paperproposal.id, :paperproposal => {:datafiles_attributes => {"0" => paperproposal_file}}
+
+    assert_redirected_to edit_paperproposal_path(@paperproposal)
+
+    get :edit, :id => @paperproposal.id
+    assert_select "div#files", {:text=> /empty_test_file/}
   end
 
   test "should not send to board if no dataset is set" do
+    login_and_load_paperproposal "Step 1 Paperproposal"
 
+    get :edit, :id => @paperproposal.id
+    assert_response :success
+
+    assert_select "form#update_state_edit", false, "Without any dataset you can not send to board"
   end
 
   test "should send to board if it is possible" do
