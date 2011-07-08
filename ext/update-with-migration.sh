f13026f8cc013667912045502665d71dae59a9ad
prilop <devnull@localhost>
2011-07-08 18:09:04 +0200

    Update with migrations script should now work as expected.

--- ext/update-with-migration.sh
+++ ext/update-with-migration.sh
@@ -1,43 +1,40 @@
 #!/bin/bash
 
 ROOT=$PWD/..
-BACKUP_FOLDER=$ROOT/backup
-
-export RAILS_ENV=staging
+RAILS_ENV=development
 
 echo "Used Ruby = `which ruby`"
 
 pushd $ROOT
 
-  echo "Updating the sources"
+  echo "==========Updating the sources=========="
   hg pull
 
-  echo "Applying the last 10 migrations"
+  echo "==========Applying the last 10 migrations=========="
   hg log --template '{rev}\n' -l 10 db/migrate/* | sort | while read line
   do
-    echo "Updating to revision $line"
-    hg update -r ${line}
+    echo "==========Updating to revision $line=========="
+    hg update -r ${line} -C 
     
-    echo "Updating gems"
-    bundle install
+    echo "==========Updating gems=========="
+    bundle install --quiet
 
-    echo "Migrating the database"
-    rake db:migrate --trace
+    echo "==========Migrating the database=========="
+    rake db:migrate
   done
 
+  echo "==========Updating to most recent revision=========="
   hg up default -C
-
-  bundle install
 
   echo `hg id -in` > tmp/hg.rev
 
-  echo "Restarting server"
+  echo "==========Restarting server=========="
   touch tmp/restart.txt
 
   echo
   sleep 5
   echo "Last 20 lines of server.log"
-  tail -vn 20 log/staging.log
+  tail -vn 20 log/$RAILS_ENV.log
 
 popd
 
