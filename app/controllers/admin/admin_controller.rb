94d93c8aa7dddc2171a249828c711ce5f0602069
Karl Welter <karlwelter@gmail.com>
2011-07-21 18:25:45 +0200

    AS for users to edit their details is now much DRYer 2

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,7 +2,8 @@ class Admin::AdminController < ApplicationController
   layout 'admin'
 
   skip_before_filter :deny_access_to_all
-  access_control do
+  access_control :admin_only_acl do
     allow :admin
   end
+
 end
--- app/controllers/settings/users_controller.rb
+++ app/controllers/settings/users_controller.rb
@@ -1,4 +1,5 @@
-class Settings::UsersController < Settings::SettingsController
+class Settings::UsersController < Admin::UsersController
+  skip_before_filter :admin_only_acl
 
   access_control do
     allow :admin
@@ -8,31 +9,12 @@ class Settings::UsersController < Settings::SettingsController
   active_scaffold :user do |config|
 
     config.label = "Your Profile"
+
     config.actions.exclude :delete, :search, :create
-    config.subform.layout = :vertical
 
-    # password column config
-    config.columns << :password
-    config.columns[:password].label = "New Password<br/>(Leave this blank to keep old password)"
-    config.columns[:password].form_ui = :password
-    config.columns << :password_confirmation
-    config.columns[:password_confirmation].label = "New Password Confirmation"
-    config.columns[:password_confirmation].form_ui = :password
+    config.show.columns << :admin
+    config.show.columns << :project_board
 
-    # show config
-    config.show.columns = [:firstname, :middlenames, :lastname, :salutation,
-        :login, :password, :password_confirmation, :comment,
-        :url, :email,
-        :institution_name, :institution_url,
-        :institution_phone, :institution_fax,
-        :street, :city, :country,
-        :admin, :project_board, :avatar]
-
-    # list config
-    config.columns = [:avatar, :firstname, :lastname, :roles_without_objects, :roles_with_objects]
-    config.list.sorting = { :lastname => :asc }
-
-    #Update config
     config.update.columns = [:firstname, :middlenames, :lastname, :salutation,
         :login, :password, :password_confirmation, :comment,
         :url, :email,
@@ -40,10 +22,5 @@ class Settings::UsersController < Settings::SettingsController
         :institution_phone, :institution_fax,
         :street, :city, :country,
         :avatar]
-
-    # for the avatar-imapge upload
-    config.update.multipart = true
-    ActiveScaffold::Bridges::Paperclip::Lib::PaperclipBridgeHelpers.thumbnail_style=:small
   end
-
 end
