86a08c64baa2fc59be40668aeb1d1ce9ec97a074
Sven Windisch <windisch@tml-consulting.net>
2011-08-04 18:09:51 +0200

    Finished dataworkbook model tests. also renamed two test files.

--- test/unit/dataworkbook_test.rb
+++ test/unit/dataworkbook_test.rb
@@ -10,7 +10,6 @@ class DataworkbookTest < ActiveSupport::TestCase
     @book = Dataworkbook.new(@dataset.upload_spreadsheet)
   end
 
-  # Replace this with your real tests.
   test "workbook was loaded correctly" do
     assert_not_nil @book
   end
@@ -24,7 +23,7 @@ class DataworkbookTest < ActiveSupport::TestCase
   end
   
   test "workbook should have four column headers" do
-    assert_equal @book.columnheaders_raw.length, 4
+    assert_equal 4, @book.columnheaders_raw.length
   end
   
   test "workbook should have unique column headers" do
@@ -32,13 +31,55 @@ class DataworkbookTest < ActiveSupport::TestCase
   end
   
   test "workbook should contain two people" do
-    assert_equal @book.people_names_hash.length, 2
+    assert_equal 2, @book.people_names_hash.length
   end
   
   test "start date of workbook should be April 18th, 2011" do
-    assert_equal @book.datemin, Date.new(2011, 4, 18)
+    assert_equal Date.new(2011, 4, 18), @book.datemin
   end
+  
   test "end date of workbook should be April 18th, 2011" do
-    assert_equal @book.datemax, Date.new(2011, 4, 18)
+    assert_equal Date.new(2011, 4, 18), @book.datemax
+  end
+  
+  test "general metadata hash should fill up correctly" do
+    assert_equal 'Test species name import', @book.general_metadata_hash[:title] 
+    assert_match /Comparative Study Plots/,  @book.general_metadata_hash[:abstract]
+    assert_match /National Forest Reserve/, @book.general_metadata_hash[:spatialextent]
+  end
+  
+  test "array of metadata column is correct" do
+    assert_equal @book.general_metadata_column.first, 'General Metadata'
+    assert_equal @book.general_metadata_column.length, 49
+  end
+
+  test "hash of people named in the workbook is correct" do
+    assert_equal 2, @book.people_names_hash.length
+    assert_equal "Karin", @book.people_names_hash.first[:firstname]
+    assert_equal "Verena", @book.people_names_hash.second[:firstname]
+  end
+  
+  test "method index for a specific columnheader is correct" do
+    assert_equal 9, @book.method_index_for_columnheader('height')
+  end
+  
+  test "column info for columnheader is correct" do
+    assert_equal 7, @book.data_column_info_for_columnheader('height').keys.length
+    assert_equal 'height in m', @book.data_column_info_for_columnheader('height')[:definition]
+  end
+  
+  test "datagroup information for columnheader is correct" do
+    assert_equal 7, @book.methodsheet_datagroup('height').keys.length
+    assert_equal 'number', @book.methodsheet_datagroup('height')[:methodvaluetype]
+  end
+  
+  test "data with head is correct for columnheader" do
+    assert_equal 5, @book.data_with_head('height').length
+    assert_equal 3.0, @book.data_with_head('height').third
+  end
+  
+  test "datagroup title for columnheader is correct" do
+    assert_equal 4,  @book.data_for_columnheader('height')[:rowmax]
+    assert_equal 'na',  @book.data_for_columnheader('height')[:data][4]
   end
 end
