f655a37b638a156f95ee55823c8ec9d84884a413
prilop <devnull@localhost>
2011-08-17 20:59:40 +0200

    ~ removed unused methods for #4838

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,20 +12,6 @@ class Project < ActiveRecord::Base
   validates_presence_of :shortname, :name
   validates_uniqueness_of :shortname, :name
 
-  ## projects should not be able to destroy, if they own data; if they acquired
-  ## download rights, those may be destroyed toghether with the projects
-  ## def before_destroy
-
-
-
-  def query_by_role(role_name)
-  #corresponding_role_ids = self.accepted_roles.all(:conditions => { :name => role_name }).map(&:id)
-
-  # this will now return all users that have +role_name+ on +object+
-  #Person.all(:conditions => ['roles.id IN (?)', corresponding_role_ids])
-  self.users.select{|person| person.has_role? role_name, self}
-  end
-
   def to_label
     "#{name}"
   end
@@ -34,12 +20,5 @@ class Project < ActiveRecord::Base
     to_label
   end
 
-
-  def user_for_role; end
-  def user_for_role=; end
-
-  def add_role_for_user
-
-  end
 
 end
