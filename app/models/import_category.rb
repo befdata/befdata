a83b69d55a4c8aa37a24d1008e6c84781daee0a8
ratcliffe <devnull@localhost>
2011-08-23 16:12:57 +0200

    ~ validate short, long and description in the import_categories

--- app/models/import_category.rb
+++ app/models/import_category.rb
@@ -1,5 +1,13 @@
 class ImportCategory < ActiveRecord::Base
   belongs_to :datacolumn
 
-  validates_presence_of :datacolumn
+  validates_presence_of :datacolumn, :short, :long, :description
+  before_validation :try_filling_missing_values
+
+  def try_filling_missing_values
+    if self.short then
+      self.long ||= self.short
+      self.description ||= self.long
+    end
+  end
 end
