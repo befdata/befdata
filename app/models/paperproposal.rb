747b7e9ad059cb28ab70c08cfe90c2bb2cd1d494
Karl Welter <karlwelter@gmail.com>
2011-11-08 14:23:10 +0100

    nothing changed, just corrected line intendation

--- app/models/paperproposal.rb
+++ app/models/paperproposal.rb
@@ -1,35 +1,35 @@
 class Paperproposal < ActiveRecord::Base
 
-    # acts_as_authorization_object :subject_class_name => 'Project'
+  # acts_as_authorization_object :subject_class_name => 'Project'
 
-    belongs_to :author, :class_name => "User", :foreign_key => "author_id"
-    belongs_to :corresponding, :class_name => "User", :foreign_key => "corresponding_id"
-    belongs_to :senior_author, :class_name => "User", :foreign_key => "senior_author_id"
+  belongs_to :author, :class_name => "User", :foreign_key => "author_id"
+  belongs_to :corresponding, :class_name => "User", :foreign_key => "corresponding_id"
+  belongs_to :senior_author, :class_name => "User", :foreign_key => "senior_author_id"
 
-    belongs_to :authored_by_project, :class_name => "Project", :foreign_key => :project_id
+  belongs_to :authored_by_project, :class_name => "Project", :foreign_key => :project_id
 
-    has_many :authors, :class_name => "User", :source => :user, :through => :author_paperproposals
-    has_many :author_paperproposals, :dependent => :destroy, :include => [:user]
+  has_many :authors, :class_name => "User", :source => :user, :through => :author_paperproposals
+  has_many :author_paperproposals, :dependent => :destroy, :include => [:user]
 
-    has_many :coordinators, :class_name => "User", :source => :person, :through => :paperproposal_votes,
-             :conditions => ['project_board_vote = ?',true]
+  has_many :coordinators, :class_name => "User", :source => :person, :through => :paperproposal_votes,
+           :conditions => ['project_board_vote = ?',true]
 
-    has_many :project_board_votes, :class_name => "PaperproposalVote",
-             :source => :paperproposal_votes, :conditions => {:project_board_vote => true }
+  has_many :project_board_votes, :class_name => "PaperproposalVote",
+           :source => :paperproposal_votes, :conditions => {:project_board_vote => true }
 
-    has_many :for_data_request_votes, :class_name => "PaperproposalVote",
-             :source => :paperproposal_votes, :conditions => {:project_board_vote => false }
+  has_many :for_data_request_votes, :class_name => "PaperproposalVote",
+           :source => :paperproposal_votes, :conditions => {:project_board_vote => false }
 
-    has_many :paperproposal_votes, :dependent => :destroy
+  has_many :paperproposal_votes, :dependent => :destroy
 
-    has_many :dataset_paperproposals, :dependent => :destroy
-    has_many :datasets, :through => :dataset_paperproposals
+  has_many :dataset_paperproposals, :dependent => :destroy
+  has_many :datasets, :through => :dataset_paperproposals
 
-    has_many :freeformats, :as => :freeformattable, :dependent => :destroy
+  has_many :freeformats, :as => :freeformattable, :dependent => :destroy
 
-    accepts_nested_attributes_for :authors
+  accepts_nested_attributes_for :authors
 
-    validates_presence_of :title, :rationale
+  validates_presence_of :title, :rationale
 
 #    def delete_all_data_groups_except(data_groups)
 #      self.data_group_data_requests.each do |data_group_data_request|
@@ -38,54 +38,54 @@ class Paperproposal < ActiveRecord::Base
 #      end
 #    end
 
-    STATES = {
-      # for the sorting
-      'accepted' => 1,
-      'in review' => 2,
-      'manuscript avaible' => 3,
-      'in prep' => 4
-    }
+  STATES = {
+    # for the sorting
+    'accepted' => 1,
+    'in review' => 2,
+    'manuscript avaible' => 3,
+    'in prep' => 4
+  }
 
-    def <=>(other)
-      # sort by state, then by title
-      x = STATES[self.state] <=> STATES[other.state]
-      x != 0 ? x : self.title.downcase <=> other.title.downcase
-    end
+  def <=>(other)
+    # sort by state, then by title
+    x = STATES[self.state] <=> STATES[other.state]
+    x != 0 ? x : self.title.downcase <=> other.title.downcase
+  end
 
-    def calc_board_state
-      return "In Preparation, no data selected yet." if self.board_state == "prep" && self.datasets.length == 0
-      return "still no aspects set" if self.board_state == "prep" && !check_aspects_for_contexts
-      return "complete" if self.board_state == "prep" && check_aspects_for_contexts
-      return "Submitted to board, waiting for acceptance." if self.board_state == "submit"
-      return "Project Board rejected your data request. Please make changes and submit again." if self.board_state == "re_prep"
-      return "accept" if self.board_state == "accept"
-      return "final" if self.board_state == "final"
-    end
+  def calc_board_state
+    return "In Preparation, no data selected yet." if self.board_state == "prep" && self.datasets.length == 0
+    return "still no aspects set" if self.board_state == "prep" && !check_aspects_for_contexts
+    return "complete" if self.board_state == "prep" && check_aspects_for_contexts
+    return "Submitted to board, waiting for acceptance." if self.board_state == "submit"
+    return "Project Board rejected your data request. Please make changes and submit again." if self.board_state == "re_prep"
+    return "accept" if self.board_state == "accept"
+    return "final" if self.board_state == "final"
+  end
 
-    def author_list
-      author_list = []
-      author_list << self.author
+  def author_list
+    author_list = []
+    author_list << self.author
 
-      middle_block = self.author_paperproposals.reject{|e| e.kind == "ack"}.map{|e| e.user}.uniq
-      ack = self.author_paperproposals.select{|e| e.kind == "ack"}.map{|e| e.user}.uniq
-      middle_block << self.corresponding
+    middle_block = self.author_paperproposals.reject{|e| e.kind == "ack"}.map{|e| e.user}.uniq
+    ack = self.author_paperproposals.select{|e| e.kind == "ack"}.map{|e| e.user}.uniq
+    middle_block << self.corresponding
 
-      middle_block = middle_block.sort{|a,b| a.lastname <=> b.lastname}
-      middle_block.reject!{|e| e == self.senior_author}
-      middle_block.reject!{|e| e == self.author}
-      middle_block.each{|e| author_list << e}
+    middle_block = middle_block.sort{|a,b| a.lastname <=> b.lastname}
+    middle_block.reject!{|e| e == self.senior_author}
+    middle_block.reject!{|e| e == self.author}
+    middle_block.each{|e| author_list << e}
 
-      author_list << self.senior_author
+    author_list << self.senior_author
 
-      ack.reject!{|e| middle_block.include?(e) ||
-                      e == self.senior_author ||
-                      e == self.author}
+    ack.reject!{|e| middle_block.include?(e) ||
+                    e == self.senior_author ||
+                    e == self.author}
 
-      ack = ack.sort{|a,b| a.lastname <=> b.lastname}
+    ack = ack.sort{|a,b| a.lastname <=> b.lastname}
 
-      hash = {:author_list => author_list, :corresponding => self.corresponding, :ack => ack}
-      return hash
-    end
+    hash = {:author_list => author_list, :corresponding => self.corresponding, :ack => ack}
+    return hash
+  end
 
   def calc_authorship(user)
      if(self.author_id==user.id)
@@ -99,21 +99,20 @@ class Paperproposal < ActiveRecord::Base
          end
        end
      end
-   end
+  end
 
-    private
+  private
 
-    def check_aspects_for_contexts
-      if self.datasets.length >= 0
-        self.dataset_paperproposals.each do |dgdr|
-          if dgdr.aspect.nil? || dgdr.aspect.empty?
-            return false
-          end
+  def check_aspects_for_contexts
+    if self.datasets.length >= 0
+      self.dataset_paperproposals.each do |dgdr|
+        if dgdr.aspect.nil? || dgdr.aspect.empty?
+          return false
         end
-        return true
       end
-      return false
+      return true
     end
-
+    return false
+  end
 
 end
