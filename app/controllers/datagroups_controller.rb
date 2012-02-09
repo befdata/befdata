37cbd8880bb12eea0595bd611c20a17e137d40b6
Karl Welter <karlwelter@gmail.com>
2012-02-09 23:47:44 +0100

    csv download for categories of datagroup. for #5163

--- app/controllers/datagroups_controller.rb
+++ app/controllers/datagroups_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class DatagroupsController < ApplicationController
 
   skip_before_filter :deny_access_to_all
@@ -13,6 +15,21 @@ class DatagroupsController < ApplicationController
 
   def show
     @datagroup = Datagroup.find params[:id]
+    respond_to do |format|
+      format.html
+      format.csv do
+        send_data render_categories_csv, :type => "text/plain", :filename=>"#{@datagroup.title}_categories.csv", :disposition => 'attachment'
+      end
+    end
+  end
+
+  def render_categories_csv
+    csv_string = CSV.generate do |csv|
+      csv << ["ID","SHORT","LONG","DESCRIPTION"]
+      @datagroup.categories.sort_by(&:short).each do |cat|
+        csv << [cat.id, cat.short, cat.long, cat.description]
+      end
+    end
   end
 
 end
