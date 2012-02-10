59809ba101a4e968578caa8c0e571ffc646e7dfd
Karl Welter <karlwelter@gmail.com>
2012-02-10 18:24:09 +0100

    removed all categories index. for #5163

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,11 @@
 class CategoriesController < ApplicationController
 
   skip_before_filter :deny_access_to_all
+
   access_control do
-    actions :index, :show do
+    actions :show do
       allow logged_in
     end
-  end
-
-  def index
-    @categories = Category.order(:short)
   end
 
   def show
