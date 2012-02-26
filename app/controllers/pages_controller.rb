d95cb30fa658b3ce6c3099c9141a5e4b330a115a
Karl Welter <karlwelter@gmail.com>
2012-02-26 20:11:50 +0100

    Gems, libraries, iconsets, aso added to imprint page. for #5261

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,29 @@ class PagesController < ApplicationController
   end
 
   def imprint
+    @external_usages = [
+      ['Rails','http://rubyonrails.org/'],
+      ['Rake','http://rake.rubyforge.org/'],
+      ['Pg','https://bitbucket.org/ged/ruby-pg'],
+      ['Haml','http://haml-lang.com/'],
+      ['Authlogic','https://github.com/binarylogic/authlogic'],
+      ['Acl9','https://github.com/be9/acl9'],
+      ['Dynamic_form','https://github.com/rails/dynamic_form'],
+      ['Paperclip','https://github.com/thoughtbot/paperclip'],
+      ['Acts-as-taggable-on','https://github.com/mbleigh/acts-as-taggable-on'],
+      ['Spreadsheet','http://spreadsheet.rubyforge.org/'],
+      ['Yaml_db','https://github.com/ludicast/yaml_db'],
+      ['Active_scaffold','https://github.com/activescaffold/active_scaffold/blob/master/MIT-LICENSE'],
+      ['Delayed_job','https://github.com/collectiveidea/delayed_job'],
+      ['Activerecord-import','https://github.com/zdennis/activerecord-import'],
+      ['Test-unit','http://test-unit.rubyforge.org/'],
+      ['Ruby-prof','http://ruby-prof.rubyforge.org/'],
+      ['PostgreSQL','http://www.postgresql.org/'],
+      ['JQuery','http://jquery.org/'],
+      ['JQuery Tablesorter','http://tablesorter.com/'],
+      ['JQuery UI','http://jqueryui.com/'],
+      ['Blueprint CSS','http://blueprintcss.org/'],
+    ]
   end
 
   def help
