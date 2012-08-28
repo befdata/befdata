f5be9b78372dbf6a7ce38fb6f93852a148fc88c1
manxingxing <manxingxing@gmail.com>
2012-08-28 14:13:32 +0800

    test processes of login and logout. for issue #33 , #23 , #45

---
+++ test/integration/user_login_logout_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class UserLoginTest < ActionDispatch::IntegrationTest
+  fixtures :all
+
+  def setup
+    @user = User.find_by_login('nadrowski')
+  end
+
+  test "login with wrong password should fail" do
+    get data_path
+    assert_response :success
+    post user_session_path, {:user_session=>{:login=>@user.login, :password=>"wrong"}},{"HTTP_REFERER"=>data_path}
+    assert_redirected_to data_path
+    follow_redirect!
+    assert_match /not/, flash[:error]
+  end
+
+  test "login with correct password should pass" do
+    get data_path
+    assert_response :success
+    post(user_session_path, {:user_session=>{:login=>@user.login, :password=>"test"}},{"HTTP_REFERER"=> data_path})
+    assert_redirected_to data_path
+    follow_redirect!
+    assert_equal "Login successful!", flash[:notice]
+  end
+
+  test "only logged-in users can visit forbidden page" do
+    get current_cart_path
+    assert_redirected_to root_path
+
+    post(user_session_path, {:user_session=>{:login=>@user.login, :password=>"test"}})
+    get current_cart_path
+    assert_response :success
+  end
+  test "logout should redirect to welcome page" do
+    post(user_session_path, {:user_session=>{:login=>@user.login, :password=>"test"}})
+    get current_cart_url
+
+    post logout_path, :method=> :delete
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "h2", /Welcome/
+  end
+
+end
