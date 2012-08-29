2db91f02e78ec95384e4fb223663f600bc77a984
Karin Nadrowski <nadrowski@uni-leipzig.de>
2012-08-29 11:58:33 +0300

    Update app/models/sheetcell.rb
    some info on datatypes and validation

--- app/models/sheetcell.rb
+++ app/models/sheetcell.rb
@@ -3,7 +3,6 @@
 #
 # The accepted value of the data is stored in the accepted_value field unless
 # the "Datatype" is a "Category" where the category id is stored instead.
-
 class Sheetcell < ActiveRecord::Base
 
   belongs_to :datacolumn
@@ -16,6 +15,8 @@ class Sheetcell < ActiveRecord::Base
     end
   end
 
+  ## Datatypes are defined in config/initializers/datatype_load.rb and the valitation procedures are 
+  ## stored in https://github.com/befdata/befdata/blob/master/db/non_schema_sql.sql
   def datatype
     if(!self.datatype_id.nil?)
       return Datatypehelper.find_by_id(self.datatype_id)
