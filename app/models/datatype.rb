9c38fb5ca71c767afeffb407d27e7469363c1e17
Karin Nadrowski <nadrowski@uni-leipzig.de>
2012-08-29 11:34:26 +0300

    Update app/models/datatype.rb
    removed writing of datatypes. Users should look into the initilization helper

--- app/models/datatype.rb
+++ app/models/datatype.rb
@@ -1,10 +1,9 @@
 ## The Datatype class is the validated data type for the "Sheetcell" accepted_value. Datatypes are initialised at application start up by the "Datatypehelper" class.
 ##
-## The possible Datatypes are text, year, number, category, date(2009-07-14) and 
-## date(14.07.2009). See config/initializers/datatype_load.rb for initializing datatypes. 
-## Datagroup contains a datatype attribute that is a suggestion for the datatype attribute
-## of Datacolumn. Datacolumn overrides Datagroup. After validation, a Datacolumn may have
-## different datatypes, since invalid values are converted to Category objects.
+## See config/initializers/datatype_load.rb ( DatattypeLoad) for initializing datatypes
+## and a list of possible datatypes. A Datagroup contains a datatype attribute that is a suggestion 
+## for the datatype attribute of a Datacolumn. Datacolumn overrides Datagroup. After validation, a 
+## Datacolumn may have different datatypes, since invalid values are converted to Category objects.
 class Datatype
   attr_accessor :id, :name, :format
 
