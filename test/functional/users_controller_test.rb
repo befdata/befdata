1f3bef641f237beb04a6eab9a93525c5b7853c4c
manxingxing <manxingxing@gmail.com>
2012-09-05 11:17:07 +0800

    A test for issue #51.
    Test logged-in user will find "Edit profile" link in his/her own page.

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-
+  setup :activate_authlogic
   test "should get index" do
     get :index
     assert_response :success
@@ -10,6 +10,17 @@ class UsersControllerTest < ActionController::TestCase
   test "should get show user" do
     get :show, {:id => User.first.path_name}
     assert_response :success
+  end
+  test "logged-in user can see 'Edit profile' in his/her page" do
+    login_user(User.first.login)
+
+    get :show, :id=>User.first.path_name
+    assert_response :success
+    assert_select "div#actions a", "Edit profile"
+
+    get :show, :id=>User.last.path_name
+    assert_response :success
+    assert_select "div#actions a", false
   end
 
 end
