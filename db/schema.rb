f8fbc6cc62243a9df4554600792cfb0c2dbcd7ff
manxingxing <manxingxing@gmail.com>
2012-09-17 11:58:23 +0800

    For issue #68. changed schema.rb after migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120827162640) do
+ActiveRecord::Schema.define(:version => 20120917015731) do
 
   create_table "author_paperproposals", :force => true do |t|
     t.integer  "paperproposal_id"
