437b316a6f8325aaa6396da76aa1522c2aa174c6
cpfaff <ctpfaff@arcor.de>
2013-01-09 12:39:23 +0100

    Removes duplication which caused the build to fail

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -54,14 +54,6 @@ class UsersControllerTest < ActionController::TestCase
     assert_success_no_error
   end 
 
-  test "user can update its api login credentials" do
-    login_nadrowski
-    old_user_credentials = User.find_by_login("nadrowski").single_access_token
-    get :update_credentials
-    new_user_credentials = User.find_by_login("nadrowski").single_access_token
-    assert_not_equal(old_user_credentials, new_user_credentials)
-  end
-
   test "show voting history" do
     login_nadrowski
     get :votes_history
