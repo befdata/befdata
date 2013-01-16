4db96324d659d4306ca589c24714bade4fe5e170
manxingxing <manxingxing@gmail.com>
2013-01-16 16:20:19 +0800

    Update to Rails 3.2.11. for issue #167

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.19'
+gem 'rails', '3.2.11'
 gem 'rake'
 gem 'pg'
 gem 'bundler'
@@ -26,10 +26,18 @@ group :test, :development do
   # gem 'ruby-prof'
   gem 'libxml-ruby'
   gem 'rb-inotify'
-end 
+end
 
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+# continous testing with guard
 group :tools do
   gem 'guard'
   gem 'guard-test'
 end
-
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,7 +7,11 @@ Befchina::Application.configure do
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
 
-  # Log error messages when you accidentally call methods on nil.
+  # Configure static asset server for tests with Cache-Control for performance
+  # config.serve_static_assets = true
+  # config.static_cache_control = "public, max-age=3600"
+
+  # Log error messages when you accidentally call methods on nil
   config.whiny_nils = true
 
   # Show full error reports and disable caching
@@ -24,6 +28,9 @@ Befchina::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+
+  # Raise exception on mass assignment protection for Active Record models
+  # config.active_record.mass_assignment_sanitizer = :strict
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
