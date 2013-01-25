39c83158e1cbf534dd52cda6d0ce6fa81d7cddc3
cpfaff <ctpfaff@arcor.de>
2013-01-25 09:16:04 +0100

    fixes function and adds end

--- app/models/datacolumn.rb
+++ app/models/datacolumn.rb
@@ -40,9 +40,9 @@ class Datacolumn < ActiveRecord::Base
     categories: {short: 'A', long: 'A', description: 'B'}
   }, using: {
     tsearch: {
-      dictionary: "english",
-      prefix: true
-    }
+    dictionary: "english",
+    prefix: true
+  }
   }
 
   def destroy_taggings
@@ -58,18 +58,18 @@ class Datacolumn < ActiveRecord::Base
   # returns the first 'count' number unique imported values
   def imported_values(count)
     values = self.sheetcells.all( :order => "import_value",
-                                  :limit => count,
-                                  :group => "import_value",
-                                  :select => "import_value")
+                                 :limit => count,
+                                 :group => "import_value",
+                                 :select => "import_value")
     return values
   end
 
   # returns the first 'count' number unique accepted values
   def accepted_values(count)
     values = self.sheetcells.all(:limit => count,
-                                  :joins => "LEFT OUTER JOIN categories ON categories.id = sheetcells.category_id" ,
-                                  :select => "distinct case when sheetcells.category_id > 0 then categories.short else sheetcells.accepted_value end as accepted_value",
-                                  :order => "accepted_value")
+                                 :joins => "LEFT OUTER JOIN categories ON categories.id = sheetcells.category_id" ,
+                                 :select => "distinct case when sheetcells.category_id > 0 then categories.short else sheetcells.accepted_value end as accepted_value",
+                                 :order => "accepted_value")
 
     return values
   end
@@ -86,20 +86,20 @@ class Datacolumn < ActiveRecord::Base
     sqlclean = "select clear_datacolumn_accepted_values(#{id})"
     # this bit will need to change once we change the column datatype to be an integer
     case self.import_data_type
-        when "text"
-          datatype = Datatypehelper.find_by_id(1)
-        when "year"
-          datatype = Datatypehelper.find_by_id(2)
-        when "date"
-          datatype = Datatypehelper.find_by_id(3)
-        when "category"
-          datatype = Datatypehelper.find_by_id(5)
-        when "number"
-          datatype = Datatypehelper.find_by_id(7)
-      else
-        #unknown
-        datatype = Datatypehelper.find_by_id(8)
-      end
+    when "text"
+      datatype = Datatypehelper.find_by_id(1)
+    when "year"
+      datatype = Datatypehelper.find_by_id(2)
+    when "date"
+      datatype = Datatypehelper.find_by_id(3)
+    when "category"
+      datatype = Datatypehelper.find_by_id(5)
+    when "number"
+      datatype = Datatypehelper.find_by_id(7)
+    else
+      #unknown
+      datatype = Datatypehelper.find_by_id(8)
+    end
     # I would like to change this so that the SQL is in one function but it wasn't working
     # TODO: I will look at this again - SR
     if(datatype.name == "text") then
@@ -150,9 +150,9 @@ class Datacolumn < ActiveRecord::Base
     #TODO check for memory consumption
     # get all the invalid sheetcells
     invalid_sheetcells = self.sheetcells.all( :order => "import_value",
-                                              :conditions => ["status_id = ?", Sheetcellstatus::INVALID],
-                                              :group => "import_value",
-                                              :select => "import_value")
+                                             :conditions => ["status_id = ?", Sheetcellstatus::INVALID],
+                                             :group => "import_value",
+                                             :select => "import_value")
 
     # No need to order the sheetcells if none were found
     return Hash.new if invalid_sheetcells.blank?
@@ -160,67 +160,67 @@ class Datacolumn < ActiveRecord::Base
     # Create a new array
     invalid_values = Hash.new
     invalid_sheetcells.each_with_index{|sc, i|
-                        invalid_values[sc.import_value] = i
-                }
+      invalid_values[sc.import_value] = i
+    }
     return invalid_values
   end
 
   # returns any invalid sheetcells with the given value
   def invalid_sheetcells_by_value(value)
     #Todo check for memory consumption
-     return self.sheetcells.all( :conditions => ["status_id = ? AND import_value=?",
-                                                 Sheetcellstatus::INVALID, value])
+    return self.sheetcells.all( :conditions => ["status_id = ? AND import_value=?",
+                               Sheetcellstatus::INVALID, value])
   end
 
   # creates a category for the invalid value and assigns the category to all matching sheetcells
   def update_invalid_value(original_value, short, long, description, user, dataset)
     # firstly check that the category doesn't already exist in the datagroup
     cat = Category.first(:conditions => ["datagroup_id = ? and short = ?",
-                                                   self.datagroup.id,
-                                                   short]
-                              )
-    if(cat.nil?)
-      cat = Category.create(:short => short,
-                          :long => long,
-                          :description => description,
-                          :status_id => Categorystatus::MANUALLY_APPROVED,
-                          :user_id => user.id,
-                          :datagroup => self.datagroup,
-                          :comment => dataset.title)
-    end
-    # update all invalid sheetcells with the same original value with the new category id
-    if(cat.valid?)
-      cells = invalid_sheetcells_by_value(original_value)
-      if(!cells.nil?)
-        cells.each do |cell|
-          cell.update_attributes(:category => cat,
-                                 :status_id => Sheetcellstatus::VALID,
-                                 :accepted_value => nil,
-                                 :datatype_id => Datatypehelper.find_by_name("category").id
-                                )
-        end
-      end
+                         self.datagroup.id,
+                         short]
+                        )
+                        if(cat.nil?)
+                          cat = Category.create(:short => short,
+                                                :long => long,
+                                                :description => description,
+                                                :status_id => Categorystatus::MANUALLY_APPROVED,
+                                                :user_id => user.id,
+                                                :datagroup => self.datagroup,
+                                                :comment => dataset.title)
+                        end
+                        # update all invalid sheetcells with the same original value with the new category id
+                        if(cat.valid?)
+                          cells = invalid_sheetcells_by_value(original_value)
+                          if(!cells.nil?)
+                            cells.each do |cell|
+                              cell.update_attributes(:category => cat,
+                                                     :status_id => Sheetcellstatus::VALID,
+                                                     :accepted_value => nil,
+                                                     :datatype_id => Datatypehelper.find_by_name("category").id
+                                                    )
+                            end
+                          end
 
-      #TODO not sure if this should really go here - Sophia
-      # update any other invalid values for columns with the same datagroup as they may contain the same values
-      columns = Datacolumn.all( :conditions => ["datagroup_id = ? and dataset_id = ?",
-                                               self.datagroup.id, self.dataset.id])
-      if(!columns.nil?)
-        columns.each do |col|
-          cells = col.invalid_sheetcells_by_value(original_value)
-          if(!cells.nil?)
-            cells.each do |cell|
-              cell.update_attributes(:category => cat,
-                                 :status_id => Sheetcellstatus::VALID,
-                                 :accepted_value => nil,
-                                 :datatype_id => Datatypehelper.find_by_name("category").id
-                                )
-            end
-          end
-        end
-      end
-    end
-    self.finished = false
+                          #TODO not sure if this should really go here - Sophia
+                          # update any other invalid values for columns with the same datagroup as they may contain the same values
+                          columns = Datacolumn.all( :conditions => ["datagroup_id = ? and dataset_id = ?",
+                                                   self.datagroup.id, self.dataset.id])
+                          if(!columns.nil?)
+                            columns.each do |col|
+                              cells = col.invalid_sheetcells_by_value(original_value)
+                              if(!cells.nil?)
+                                cells.each do |cell|
+                                  cell.update_attributes(:category => cat,
+                                                         :status_id => Sheetcellstatus::VALID,
+                                                         :accepted_value => nil,
+                                                         :datatype_id => Datatypehelper.find_by_name("category").id
+                                                        )
+                                end
+                              end
+                            end
+                          end
+                        end
+                        self.finished = false
   end
 
   # this should not be happen but we thought it might be a good last step before we can
@@ -272,11 +272,12 @@ class Datacolumn < ActiveRecord::Base
     else
       return false
     end
+  end
 
   # acl9 related stuff: users
 
   # users of a datacolumn are those who are responsible for it
-  def users= (people)
+  def users(people)
     self.set_user_with_role(:responsible, people)
   end
 end
