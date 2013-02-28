79ebbeb1fb78af10e65effc3e213375616e8c485
cpfaff <ctpfaff@arcor.de>
2013-02-28 11:56:13 +0100

    Change error notice to make it more clear to the user what happened.

--- app/models/datafile.rb
+++ app/models/datafile.rb
@@ -12,7 +12,7 @@ class Datafile < ActiveRecord::Base
   :basename => "basename",
   :path => ":rails_root/files/:id_:filename",
   :url => "/files/:id/download"
-  
+
   def basename
     return File.basename(self.file.original_filename, File.extname(self.file.original_filename))
   end
@@ -28,7 +28,7 @@ class Datafile < ActiveRecord::Base
       ss = Spreadsheet.open(file_path)
       ss.io.close
     rescue
-      errors.add :file, "can not be read"
+      errors.add :file, "Sorry file could not be read. We currently only support uploading .xls files."
       return
     end
 
