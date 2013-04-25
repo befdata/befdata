59cd373425b670d867ee8e7dcd3aa23938db207f
manxingxing <manxingxing@gmail.com>
2013-04-25 13:29:29 +0800

    + Add accept_invalid_values function to database. related to issue #192 #186

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130419092407) do
+ActiveRecord::Schema.define(:version => 20130425045913) do
 
   create_table "author_paperproposals", :force => true do |t|
     t.integer  "paperproposal_id"
