b5e314c6bab24c3d31d6078b71083df552b3ed2c
manxingxing <manxingxing@gmail.com>
2013-07-26 09:55:47 +0800

    fix typo

--- test/integration/upload_a_dataset_test.rb
+++ test/integration/upload_a_dataset_test.rb
@@ -9,7 +9,7 @@ class UploadADatasetTest < ActionDispatch::IntegrationTest
 
     uploadedfile = test_file_for_upload("UnitTestSpreadsheetForUpload_new.xls")
 
-    post create_with_workbook_datasets_path, {:datafile => {:file => uploadedfile}}
+    post create_with_datafile_datasets_path, {:datafile => {:file => uploadedfile}}
     #now it's on views/create pages
     assert_nil flash[:error]
     assert_template :create
