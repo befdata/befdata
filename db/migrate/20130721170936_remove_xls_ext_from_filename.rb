ce20e64ec7a18d8638cab5f13708536af020e666
manxingxing <manxingxing@gmail.com>
2013-08-25 12:26:45 +0800

    replace update_attribute with update_column in a migration file so that it doesn't trigger data regeneration. issue #322

--- db/migrate/20130721170936_remove_xls_ext_from_filename.rb
+++ db/migrate/20130721170936_remove_xls_ext_from_filename.rb
@@ -1,7 +1,7 @@
 class RemoveXlsExtFromFilename < ActiveRecord::Migration
   def up
     Dataset.where('filename is not NULL').each do |dt|
-      dt.update_attribute(:filename, File.basename(dt.filename, '.xls'))
+      dt.update_column(:filename, File.basename(dt.filename, '.xls'))
     end
   end
 
