0ac4e3156cf01cbcc27f201e66a9562d1bd3b8bc
manxingxing <manxingxing@gmail.com>
2014-08-19 00:33:24 +0800

    tabbing in staff page. #376

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.select('id, firstname, lastname, salutation, email, avatar_file_name, alumni')
-                  .order('alumni, lower(lastname) asc, lower(firstname) asc')
+                  .order('lower(lastname) asc, lower(firstname) asc')
     respond_to do |format|
       format.html
       format.xml
