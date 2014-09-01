04b4f61e033b543818557a2c96fc9f9cd0d3bc58
manxingxing <manxingxing@gmail.com>
2014-09-02 00:17:21 +0800

    After the user is successfully logged in, redirect back to the last request url.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -15,7 +15,12 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
-      redirect_back_or_default root_url
+      if session[:return_to]
+        redirect_to session[:return_to]
+        session.delete :return_to
+      else
+        redirect_back_or_default root_url
+      end
     else
       flash[:error] = @user_session.errors.full_messages.to_sentence
       redirect_back_or_default root_url
