7b9794b925f10716fc080d1a6ae232dda38e401f
manxingxing <manxingxing@gmail.com>
2014-11-09 15:59:14 +0800

    deleget NullGroup’s methods to a new datagroup(Datagroup.new) object

--- app/models/null_datagroup.rb
+++ app/models/null_datagroup.rb
@@ -4,15 +4,10 @@ class NullDatagroup
   def empty?; true; end
   def !; true; end
 
-  def categories; []; end
-
-  Datagroup.attribute_names.each do |f|
-    define_method f do
-      nil
-    end
-  end
-
   def self.decrement_counter(counter_name, id); ; end
   def self.increment_counter(counter_name, id); ; end
 
+  def method_missing(method_name, *args)
+    Datagroup.new.send method_name, *args
+  end
 end
